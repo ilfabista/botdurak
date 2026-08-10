933d460b6faaec27e9fbc715cfab08d904755368
Fabio <fabio@local>
2026-08-11 01:53:44 +0200

    config.js: nuovo tunnel cloudflared (flowers-pocket-begin-front)

--- webapp/config.js
+++ webapp/config.js
@@ -6,4 +6,4 @@
    l'URL stabile, es. wss://durak-backend.onrender.com).
    Un parametro ?ws=... nell'URL ha la precedenza su questa impostazione.
 */
-window.DURAK_CONFIG = { wsUrl: "wss://alaska-historical-southeast-green.trycloudflare.com" };
+window.DURAK_CONFIG = { wsUrl: "wss://flowers-pocket-begin-front.trycloudflare.com" };
